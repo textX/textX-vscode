908b6e1ef6453e4348eb2d4fe9e4ed3cad7d3516
Elero Daniel <danixeee@gmail.com>
2018-03-07 19:05:04 +0100

    Changed extension activator function

--- src/extension.ts
+++ src/extension.ts
@@ -7,13 +7,17 @@
 import * as net from 'net';
 import * as vscode from 'vscode';
 import { Disposable, ExtensionContext, workspace } from 'vscode';
-import { ExecutableOptions, ServerOptions, LanguageClient, LanguageClientOptions } from 'vscode-languageclient';
+import { ExecutableOptions, ServerOptions, LanguageClient, LanguageClientOptions, StreamInfo } from 'vscode-languageclient';
 
 import { join } from 'path';
 import * as fs from 'fs';
 import { setTimeout } from 'timers';
 import { CodeOutline } from './outline/outline';
- 
+
+import * as cp from "child_process"
+import * as portfinder from "portfinder";
+
+
 function startLangServer(command: string, args: string[], documentSelector: string[]): Disposable {
 
 	const options: ExecutableOptions  = {
@@ -26,9 +30,9 @@ function startLangServer(command: string, args: string[], documentSelector: stri
 	};
 	const clientOptions: LanguageClientOptions = {
 		documentSelector: ['*'],
-        synchronize: {
-            configurationSection: command
-        }
+		synchronize: {
+			fileEvents: workspace.createFileSystemWatcher('**/*.*')
+		}
 	}
 	return new LanguageClient(command, serverOptions, clientOptions).start();
 }
@@ -55,66 +59,112 @@ function startLangServerTCP(addr: number): Disposable {
 	return new LanguageClient(`tcp lang server (port ${addr})`, serverOptions, clientOptions).start();
 }
 
+var PROCESS: cp.ChildProcess = null;
 export function activate(context: ExtensionContext) {
+	
+	findPythonExecutable().then(python => {
 
-	// Path to virtualenv where textx language server is installed - from configuration section
-	const pyEnvPath : string = vscode.workspace.getConfiguration().get('textxls.env');
+		if (!python){
+			vscode.window.showWarningMessage("Python 3 is required!");
+			return;
+		}
 
-	// Path to __main__.py module inside the extension
-	// When deploying extension, copy https://github.com/textX-tools/textX-languageserver/tree/master/textx_langserv
-	// in root of this extension
-	const pyMainPath : string = join(__dirname,'../textx-languageserver/textx_langserv/__main__.py')
+		// Path to __main__.py module inside the extension
+		const textxls_main : string = join(__dirname,'../textX-languageserver/__main__.py')
 
-	// Path to installation guide file
-	const instGuidePath : string = join(__dirname,'../installation-guide.txt')
+		let lsDisp: Disposable = null;
 
-	// Add error callback when spawning process in language-client
-	// On error, open installation guide
-	function openInstallationGuide() {
-		// Local installation guide
-		let guideTxt = fs.readFileSync(instGuidePath, 'utf8');
-		// Open installation guide in new tab
-		vscode.workspace.openTextDocument(instGuidePath).then(doc=> {
-			vscode.window.showTextDocument(doc);                   
-	   	});
+		// START FOR PUBLISH
+		lsDisp = startLangServer(python, [textxls_main], []);
+
+		// START CLIENT TCP
+		// lsDisp = startLangServerTCP(5000);
+		context.subscriptions.push(lsDisp);
+
+		registerCodeOutline(context);
+		registerLensReferences(context);
+
 		
-	}
-
-	let lsDisp: Disposable = null;
-	// Configuration section exists
-	// Comment when debugging via tcp
-	// if (pyEnvPath){
-	// 	lsDisp = startLangServer(pyEnvPath, [pyMainPath], []);
-	// }
-	// // Language server is installed globaly
-	// else{
-	// 	lsDisp = startLangServer('textxls', [], []);
-	// }
-
-	// Uncomment for tcp
-	lsDisp = startLangServerTCP(5000);
-
-	context.subscriptions.push(lsDisp);
-	
-
-	// Code outline
-	let disp = vscode.workspace.onDidOpenTextDocument((doc) => {
-		new CodeOutline(context);
+		// START SERVER AND CLIENT TCP
+		// startServerAndClientTCP(python, textxls_main, context).then((proc) => {
+		// 	PROCESS = proc;
+		// });
 	});
+}
 
-	context.subscriptions.push(disp);
-	
 
-	// Code lens
+function startServerAndClientTCP(python: string, textxls_main: string, context: vscode.ExtensionContext): Promise<cp.ChildProcess> {
+	return new Promise((resolve, reject) => {
+
+		portfinder.getPortPromise().then((port) => {
+			let command = python + ' ' + textxls_main + ' --tcp --port ' + port.toString();
+			let process = cp.exec(command);
+
+			if (process.pid){
+				let client = startLangServerTCP(port);
+				context.subscriptions.push(client);
+
+				registerCodeOutline(context);
+				registerLensReferences(context);
+				
+				resolve(process);
+			}
+
+		});
+
+	});
+}
+
+function registerLensReferences(context: vscode.ExtensionContext): void{
 	vscode.commands.registerCommand('code_lens_references', (...args) => {
 		// Change cursor position
 		const editor = vscode.window.activeTextEditor;
-        const position = editor.selection.active;
-        var newPosition = position.with(args[0], args[1]);
-        var newSelection = new vscode.Selection(newPosition, newPosition);
+		const position = editor.selection.active;
+		var newPosition = position.with(args[0], args[1]);
+		var newSelection = new vscode.Selection(newPosition, newPosition);
 		editor.selection = newSelection;
 		// Trigger find all references command
 		vscode.commands.executeCommand('editor.action.referenceSearch.trigger');
 	});
+}
 
+function registerCodeOutline(context: vscode.ExtensionContext): void{
+	let disp = vscode.workspace.onDidOpenTextDocument((doc) => {
+		new CodeOutline(context);
+	});
+	context.subscriptions.push(disp);
+}
+
+function findPythonExecutable() : Promise<string>{
+	const python = 'python';
+	const python3 = 'python3';
+
+	return new Promise((resolve, reject) => {
+        cp.execFile(python, ['--version'], { }, (error, stdout, stderr) => {
+			let version = getPythonVersion(stdout);
+			if (version && version[0] === '3'){
+				resolve(python);
+			}else{
+				return cp.execFile(python3, ['--version'], { }, (error, stdout, stderr) => {
+					let version = getPythonVersion(stdout);
+					if (version){
+						resolve(python3);
+					}else{
+						resolve(null);
+					}
+				});
+			}
+		});
+	});
+	
+	function getPythonVersion(stdout){
+		let regArray = new RegExp('\\d.\\d.\\d').exec(stdout);
+		return regArray != null ? regArray[0] : null;
+	}
+}
+
+export function deactivate() {
+	// For TCP
+	if (PROCESS)
+		PROCESS.kill('SIGHUP');
 }
